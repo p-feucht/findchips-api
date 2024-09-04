af1d3fd1b73493e21f8e5e6ad21682cbf83e26d0
Philipp Schnalke <philipp.schnalke@zf.com>
2024-09-04 15:47:49 +0200

    change to class and init function

--- findchipsScraper.py
+++ findchipsScraper.py
@@ -1,19 +1,20 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-
-# Call findchips with search parameter
-def search_chip(part_number):
-    url="https://www.findchips.com/search/"
-    page = urlopen(url+part_number)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    results = soup.find_all(class_='distributor-results')
-    filename = "chip.html"
-    with open(filename, "w", encoding="utf-8") as file:
-        for result in results:
-            file.write(str(result.contents[3]))
-    file.close()
-    return file
+class chip:
+    # Call findchips with search parameter
+    def search_chip(self):
+        url="https://www.findchips.com/search/"
+        page = urlopen(url+self.part_number)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        results = soup.find_all(class_='distributor-results')
+        filename = "chip.html"
+        with open(filename, "w", encoding="utf-8") as file:
+            for result in results:
+                file.write(str(result.contents[3]))
+        file.close()
+        return file
     
-#search_chip("sn75468d")
+    def __init__(self, part_number):
+        self.part_number = part_number
